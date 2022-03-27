2648a21222f7bc66e38106cf1d42e4be459e258c
Sebastian Kulaj <virez24@icloud.com>
2022-03-27 18:40:46 +0200

    Quick update

--- sum.go
+++ sum.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func main() {
+
+	fmt.Println("Hi it is a simple app for DevOps class")
 }
